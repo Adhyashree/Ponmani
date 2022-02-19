db63d160869433d7332105d2b67e5cc95593b04c
Ponmani <99885506+Adhyashree@users.noreply.github.com>
2022-02-19 20:23:51 +0530

    Add files via upload

---
+++ datatype1.py
@@ -0,0 +1,64 @@
+"""  IN THIS PROGRAM AM USING
+      VARIOUS DATA TYPE VALUE
+      FINDING  VARIABLE TYPE
+      COMMENT STATEMENT
+      PRINT FUNCTION  """
+print("WELCOME TO THIS PROGRAM ")
+'Assign value to the Variable '
+ #assign numeric value to the variable
+a=10   #integer type
+b=55.25 # Floating type
+c=10+23j # complex type
+
+#assign sequence type  vale to the variable
+name="ponmani"  #string type
+ #LIST
+list_integer=[25,23,5,55] #list of integer values
+list_float=[12.3,45.7,8.9]  # list of floating values
+list_str=["pon","adhya","balaji"]  # list of string values
+list_complex=[1+28j,17+8j,45+99j]  #list of complex values
+list_mixed=[1,7.9,67+4j,"pon"] # list of int,float, str,complex  items
+ #TUPLE
+tuple_integer=(25,23,5,55) #tuple of integer values
+tuple_float=(12.3,45.7,8.9)  # tuple of floating values
+tuple_str=("pon","adhya","balaji")  # tuple of string values
+tuple_complex=(1+28j,17+8j,45+99j)  #tuple of complex values
+tuple_mixed=(1,7.9,67+4j,"pon") # tuple of int,float, str,complex  items
+
+
+# Print the variable data type
+print(type(a))
+print(type(b))  
+print(type(c))
+print(type(name))  
+print(type(list_integer)) 
+print(type(list_float)) 
+print(type(list_str)) 
+print(type(list_complex))  
+print(type(list_mixed)) 
+print(type(tuple_integer))
+print(type(tuple_float))
+print(type(tuple_complex))
+print(type(tuple_str))
+print(type(tuple_mixed))
+
+#Print the variable value
+print(a)      
+print(b)
+print (c)
+print(name)
+
+#PRINT THE LIST VALUES
+print(list_integer)
+print(list_float)
+print(list_complex)
+print(list_str)
+print(list_mixed)
+
+#PRINT THE TUPLE VALUES
+print(tuple_integer)
+print(tuple_float)
+print(tuple_complex)
+print(tuple_str)
+print(tuple_mixed)
+#End of the progeram
