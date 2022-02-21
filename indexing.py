3e5c1a942eda484e93cec10fa46c7fe199f0c6dc
Ponmani <99885506+Adhyashree@users.noreply.github.com>
2022-02-21 14:00:47 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,19 @@
+#program about indexing
+#string
+name=input("enter the name\n")
+print(name[2])
+print(name[-2])
+print(name[0:4])
+print(name[:])
+print(name[:2])
+print(name[1:])
+print(name[1:-1])
+#indexing-list
+list_names=["aa","bb","cc"]
+print(list_names[2])
+print(list_names[-2])
+#indexing-tuple
+tuple_name=(12,4.5,"str",23j)
+print(tuple_name[3])
+print(tuple_name[2][1])
+#end
